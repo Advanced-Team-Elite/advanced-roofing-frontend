25faf72f9270e42f83f966a17a94860d447e48e3
dtalledo18 <dtalledo@advancedteamelite.com>
2026-03-02 11:30:17 -0500

    add: next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const nextConfig = {
+    async redirects() {
+        return [
+            {
+                source: '/:path*',
+                has: [
+                    {
+                        type: 'header',
+                        key: 'x-forwarded-proto',
+                        value: 'http',
+                    },
+                ],
+                permanent: true,
+                destination: 'https://advancedteamelite.com/:path*',
+            },
+        ]
+    },
+}
 
 export default nextConfig;
